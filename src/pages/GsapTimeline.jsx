415924eeb86036e420b624c3b440ef6489737fe3
Suraj Nair <surajnair9698@gmail.com>
2026-02-20 21:00:44 +0530

    feat: add basic gsap.timeline() animation to div and also implement the play pause functionality

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,6 +1,46 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  const timeline = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 0,
+    yoyo: true,
+  });
 
+  useGSAP(() => {
+    timeline.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      duration: 2,
+      borderRadius: "100%",
+      ease: "back.inOut",
+    });
+
+    timeline.to("#yellow-box", {
+      x: 500,
+      rotation: 360,
+      duration: 2,
+      borderRadius: "0%",
+      ease: "back.inOut",
+    });
+    timeline.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      duration: 2,
+      borderRadius: "100%",
+      ease: "back.inOut",
+    });
+    timeline.to("#yellow-box", {
+      y: 100,
+      rotation: 360,
+      duration: 2,
+      scale: 2,
+      borderRadius: "100%",
+      ease: "back.inOut",
+    });
+  }, []);
   return (
     <main>
       <h1>GsapTimeline</h1>
@@ -35,7 +75,17 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button
+          onClick={() => {
+            if (timeline.paused()) {
+              timeline.play();
+            } else {
+              timeline.pause();
+            }
+          }}
+        >
+          Play/Pause
+        </button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
