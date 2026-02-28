b67f3809ffa19ff7d287c85923c5a44982d405fc
Suraj Nair <surajnair9698@gmail.com>
2026-02-28 18:03:15 +0530

    feat: implement GSAP stagger animation using useGSAP hook

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,6 +1,21 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
-
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 50,
+      rotation: 360,
+      borderRadius: "100%",
+      stagger: {
+        amount: 1.5,
+        grid: [1, 2],
+        axis: "x",
+        ease: "circ.in",
+        from: "end",
+      },
+    });
+  });
   return (
     <main>
       <h1>GsapStagger</h1>
@@ -31,14 +46,11 @@ const GsapStagger = () => {
       </p>
 
       <div className="mt-20">
-        <div className="flex gap-5">
+        <div className="flex justify-center gap-5">
           <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-500 rounded-lg stagger-box" />
-          <div className="w-20 h-20 bg-indigo-600 rounded-lg stagger-box" />
-          <div className="w-20 h-20 bg-indigo-700 rounded-lg stagger-box" />
-          <div className="w-20 h-20 bg-indigo-800 rounded-lg stagger-box" />
         </div>
       </div>
     </main>
